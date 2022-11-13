eec23a07fe43c5049d9a05c2e5fbab3159351593
Pwsjas <pwsjas433@hotmail.com>
2022-11-12 23:45:19 -0500

    fix display bug for players with less than 8 keys

--- app/src/app/dungeon-widget/dungeon-widget.component.ts
+++ app/src/app/dungeon-widget/dungeon-widget.component.ts
@@ -23,26 +23,32 @@ export class DungeonWidgetComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.dungeon);
-    this.dungeonName = this.dungeon.fort.short_name;
-    this.dungeonLevelFort = Number(this.dungeon.fort.mythic_level);
-    this.dungeonLevelTyran = Number(this.dungeon.tyran.mythic_level);
-    this.dungeonUpgradeFort = this.dungeon.fort.num_keystone_upgrades;
-    this.dungeonUpgradeTyran = this.dungeon.tyran.num_keystone_upgrades;
+    if (this.dungeon.fort) {
+      this.dungeonName = this.dungeon.fort.short_name;
+      this.dungeonLevelFort = Number(this.dungeon.fort.mythic_level);
+      this.dungeonUpgradeFort = this.dungeon.fort.num_keystone_upgrades;
 
-    if (Number(this.dungeonUpgradeFort) === 1) {
-      this.plusFort = "+"
-    } else if (Number(this.dungeonUpgradeFort) === 2) {
-      this.plusFort = "++"
-    } else if (Number(this.dungeonUpgradeFort) === 3) {
-      this.plusFort = "+++"
+      if (Number(this.dungeonUpgradeFort) === 1) {
+        this.plusFort = "+"
+      } else if (Number(this.dungeonUpgradeFort) === 2) {
+        this.plusFort = "++"
+      } else if (Number(this.dungeonUpgradeFort) === 3) {
+        this.plusFort = "+++"
+      }
     }
 
-    if (Number(this.dungeonUpgradeTyran) === 1) {
-      this.plusTyran = "+"
-    } else if (Number(this.dungeonUpgradeTyran) === 2) {
-      this.plusTyran = "++"
-    } else if (Number(this.dungeonUpgradeTyran) === 3) {
-      this.plusTyran = "+++"
+    if (this.dungeon.tyran) {
+      this.dungeonLevelTyran = Number(this.dungeon.tyran.mythic_level);
+      this.dungeonUpgradeTyran = this.dungeon.tyran.num_keystone_upgrades;
+
+
+      if (Number(this.dungeonUpgradeTyran) === 1) {
+        this.plusTyran = "+"
+      } else if (Number(this.dungeonUpgradeTyran) === 2) {
+        this.plusTyran = "++"
+      } else if (Number(this.dungeonUpgradeTyran) === 3) {
+        this.plusTyran = "+++"
+      }
     }
 
     this.averageRating = Math.round(this.averageRating);
